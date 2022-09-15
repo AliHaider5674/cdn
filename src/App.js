bae5c5d050d05527cd92de609b14a74202862f5e
alihaider <alehaider5674@gmail.com>
2022-09-15 17:45:27 +0500

    assw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { Amplify, Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(awsconfig);
 function App({ signOut, user }) {
   return (
     <>
